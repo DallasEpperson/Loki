01dc421e62ab9f3996e3543512edc8c030b6defa
Dallas Epperson <depperson@pcgus.com>
2020-12-31 10:08:46 -0500

    Got a little Ooish

--- main.js
+++ main.js
@@ -1,26 +1,18 @@
-const { app, BrowserWindow } = require('electron');
+'use strict';
 
-function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
+const { app } = require('electron');
+const path = require('path');
+const Window = require('./Window');
+
+function main(){
+    //todo get window size from last invocation
+    let mainWindow = new Window({
+        file: path.join('renderer', 'index.html')
     });
-    win.loadFile('renderer/index.html');
-}
+};
 
-app.whenReady().then(createWindow);
+app.on('ready', main);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+    app.quit();
 });
