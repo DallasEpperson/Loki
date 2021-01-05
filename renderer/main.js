6d94b2425e0827f395355b7dbaaee9bf02cad581
Dallas Epperson <depperson@pcgus.com>
2021-01-05 16:33:55 -0500

    Rendering item name in main area when item is loaded

--- renderer/main.js
+++ renderer/main.js
@@ -9,7 +9,7 @@ $('#back').on('click', ()=>{
 
 ipcRenderer.on('item-details-response', (_event, details) => {
     console.log('item-details-response', details);
-    //TODO render main section of page
+    // Item containers
     const containersDiv = $('#item-containers');
     containersDiv.empty();
     if(details.parents && details.parents.length > 0){
@@ -24,13 +24,20 @@ ipcRenderer.on('item-details-response', (_event, details) => {
         if(!itemId) return;
         ipcRenderer.send('item-details-get', {itemId: itemId});
     });
+
+    // Item main
+    $('#item-main h1').text(details.name);
 });
 
 ipcRenderer.on('item-list-updated', (_event, items) => {
     console.log('item-list-updated', items);
     const itemList = $('#item-list');
     itemList.empty();
-    items.forEach(item => {
+    items.sort((a,b)=> {
+        if(a.name.toUpperCase() < b.name.toUpperCase()) {return -1;}
+        if(a.name.toUpperCase() > b.name.toUpperCase()) {return 1;}
+        return 0;
+    }).forEach(item => {
         itemList.append(`<li data-id="${item.id}">${item.name}</li>`);
     });
     $('#item-list li').on('click', function(){
