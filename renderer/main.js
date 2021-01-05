e8bb3ed94fd674deaa928a7b94c838cb07e02726
Dallas Epperson <depperson@pcgus.com>
2021-01-05 15:46:36 -0500

    container "breadcrumb" now rendering and working

--- renderer/main.js
+++ renderer/main.js
@@ -6,6 +6,20 @@ window.$ = window.jQuery = require('jquery');
 ipcRenderer.on('item-details-response', (_event, details) => {
     console.log('item-details-response', details);
     //TODO render main section of page
+    const containersDiv = $('#item-containers');
+    containersDiv.empty();
+    if(details.parents && details.parents.length > 0){
+        details.parents.sort((a,b) => {return b.level - a.level})
+        .forEach(parent => {
+            containersDiv.append(`<a data-id="${parent.id}">${parent.name}</a>`);
+        });
+    }
+    containersDiv.append(`<a>${details.name}</a>`);
+    $('#item-containers a').on('click', function(){
+        const itemId = parseInt($(this).data('id'));
+        if(!itemId) return;
+        ipcRenderer.send('item-details-get', {itemId: itemId});
+    });
 });
 
 ipcRenderer.on('item-list-updated', (_event, items) => {
